69bccbbe09872b55d14e611e2345f5041cd458db
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2022-11-15 23:34:26 +0100

    add - profil

---
+++ widgets/Posts/Info.jsx
@@ -0,0 +1,80 @@
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import * as inline_heart from '@fortawesome/free-regular-svg-icons/faHeart';
+import * as solid_heart from '@fortawesome/free-solid-svg-icons/faHeart';
+import * as inline_comment from '@fortawesome/free-regular-svg-icons/faComment';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+
+const { width, height } = Dimensions.get('window');
+
+library.add(inline_comment.faComment, solid_heart.faHeart, inline_heart.faHeart);
+
+const PostInfo = (props) => {
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={['transparent', 'rgba(0,0,0,0.8)', 'rgba(0,0,0,1)']}
+    >
+      <View style={styles.left}>
+        <Image style={styles.picture} source={{ uri: props.user.picture }} resizeMode='cover' />
+      </View>
+      <View style={styles.mid}>
+        <Text style={styles.username}>@{props.user.username}</Text>
+        <Text style={styles.location}>Avenue de QuelquePart</Text>
+      </View>
+      <View style={styles.right}>
+        <FontAwesomeIcon style={styles.icon} icon={solid_heart.faHeart} color="red" size={30} />
+        <FontAwesomeIcon style={styles.icon} icon={inline_comment.faComment} color="white" size={30} />
+      </View>
+    </LinearGradient>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 9 / 10 * width,
+    height: width / 7,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: "absolute",
+    bottom: 0,
+    borderRadius: 20,
+  },
+  left: {
+    width: width / 8,
+    marginHorizontal: 15
+  },
+  picture: {
+    width: width / 8,
+    height: width / 8,
+    borderRadius: 90
+  },
+  mid: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  username: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'left'
+  },
+  location: {
+    color: 'grey'
+  },
+  right: {
+    width: width / 20,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginRight: 10
+  },
+  icon: {
+    margin: 5,
+  }
+});
+
+export default PostInfo;
---
+++ widgets/Posts/Post.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import PostInfo from "./Info";
+
+const { width, height } = Dimensions.get('window');
+
+
+const Post = (props) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.post} source={{uri: props.post}} resizeMode='cover'/>
+      <PostInfo user={props.user} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  post: {
+    width: 9/10 * width,
+    height: 9/10 * width,
+    borderRadius: 20
+  }
+});
+
+export default Post;
