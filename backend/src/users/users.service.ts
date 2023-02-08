d1196ecb0d2d6f0f6db4812fdc7164ea0e8a2f6d
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2023-02-08 10:58:02 +0100

    fix - error on not found user

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { catchError, from, map, Observable, throwError } from 'rxjs';
 import { UserDbo } from 'src/dbo/user.dbo';
@@ -15,7 +15,10 @@ export class UsersService {
 
   findOne(username: string): Observable<User> {
     return from(this.repo.findOneBy({ username })).pipe(
-      map((dbo: UserDbo) => UserMapper.fromDbo(dbo)),
+      map((dbo: UserDbo) => {
+        if(dbo === null) throw new NotFoundException(`No user with username '${username}'`)
+        return UserMapper.fromDbo(dbo);
+      }),
       catchError(err => {
         console.warn(err.message);
         return throwError(err);
